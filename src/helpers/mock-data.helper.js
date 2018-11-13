2e23e4f48c290385db38af1226db13a6f2578ace
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2018-11-13 12:27:33 +0800

    add mock-data for event detail

---
+++ src/helpers/mock-data.helper.js
@@ -0,0 +1,61 @@
+export const event = {
+  "eventId": "86b56676-231f-4302-b372-c03ef837c706",
+  "imageUrl": "https://fileservice.petronas.com/api/v1/view/image/2d34c3d2-d509-46cf-a7ea-e1cfdb237271-jom-hangatkan-cub-prix-2017-di-pahang-2017030.png",
+  "eventName": "Jom Hangatkan Cub Prix 2017 di Pahang!",
+  "eventDescription": "Jumpa anda di PETRONAS AAM Malaysian Cub&nbsp;Prix 2017. Jumpa anda di PETRONAS AAM Malaysian Cub&nbsp;Prix 2017. Jumpa anda di PETRONAS AAM Malaysian Cub&nbsp;Prix 2017. Jumpa anda di PETRONAS AAM Malaysian Cub&nbsp;Prix 2017. Jumpa anda di PETRONAS AAM Malaysian Cub&nbsp;Prix 2017. Jumpa anda di PETRONAS AAM Malaysian Cub&nbsp;Prix 2017",
+  "dateFrom": "2017-03-04T02:00:00",
+  "dateTo": "2017-03-05T10:00:00",
+  "gmt": "+08:00",
+  "eventLocation": {
+    "locationId": "0481847f-67b8-4df8-a78e-8c5cde379312",
+    "locationName": "Temerloh",
+    "formattedAddress": "Temerloh , ",
+    "latitude": 3.448649,
+    "longitude": 102.4163482
+  },
+  "eventCategories": [
+    {
+      "categoryId": "059d6a06-9076-4d6c-8bf2-0f8ec8868cb1",
+      "categoryName": "Sports"
+    }
+  ],
+  "eventOrganizers": [
+    {
+      "isSuperAdmin": false,
+      "objectGUID": "609a2593-284f-4de3-9bf6-6aef548a4374",
+      "displayName": "Wan Noor Maheran Bt Majidin (GSC/PETH)",
+      "email": "noormaheran.majidin@petronas.com.my",
+      "sAMAccountName": "noormaheran.majidin",
+      "company": "Petroliam Nasional Berhad",
+      "department": "Media Engagement",
+      "division": "Group Strategic Communications",
+      "position": "Executive (Social Media)",
+      "office": "PETT L70 T1",
+      "telephoneNumber": "+603 - 2331 9135",
+      "employeeNumber": "1005944",
+      "thumbnailPhoto": "https://oexstorage.blob.core.windows.net/fileservices/UserPhotos/PETRONAS_noormaheran.majidin.jpg",
+      "isFirstLogon": false,
+      "isFirstJoin": false,
+      "domain": "PETRONAS",
+      "isExternal": false,
+      "pwdLastSet": "2018-09-18T15:52:20.3891435"
+    }
+  ],
+  "seatLeft": 0,
+  "seatTotal": 0,
+  "seatAttendees": 0,
+  "seatWaitList": 0,
+  "seatPendingList": 0,
+  "checkedInTotal": 0,
+  "eventStatus": "PAST",
+  "isFeatured": false,
+  "isScreening": false,
+  "eventType": "Public",
+  "slugName": "Jom-Hangatkan-Cub-Prix-2017-di-Pahang",
+  "isCheckedIn": false,
+  "isOrganizer": false,
+  "isSuperAdmin": false,
+  "isBookmark": false,
+  "userStatus": "NEW",
+  "isEmailTicket": false
+}
