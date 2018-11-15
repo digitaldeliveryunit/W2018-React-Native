a418ac063b64dff2140e7f14c6bbdee46c70db51
Khoa Tran Anh <khoa.trananh@petronas.com>
2018-11-15 16:06:06 +0800

    UI for spotlight swipe

--- src/components/SpotlightCard.js
+++ src/components/SpotlightCard.js
@@ -19,69 +19,75 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12
   },
   wrapperItem: {
-    flexDirection: "column",
     backgroundColor: "#FFF",
     alignItems: "center",
-    width: WIDTH_SPOTLIGHT_ITEM,
     borderRadius: 9,
-    margin: 10,
+    margin: 0,
     shadowColor: "#060606",
     shadowOffset: {
       width: 0,
-      height: 3
+      height: 0
     },
-    shadowRadius: 9,
     shadowOpacity: 0.1
   },
   imageContainer: {
+    width: "100%",
     overflow: "hidden",
     borderTopLeftRadius: 9,
     borderTopRightRadius: 9
   },
-  image: {
-    width: WIDTH_SPOTLIGHT_ITEM,
-    height: 92
-  },
-  lblName: {
+  description: {
+    borderTopWidth: 1,
+    borderTopColor: "#F3F3F3",
+    width: "90%",
     marginTop: 10,
-    paddingHorizontal: 25,
-    alignSelf: "center",
-    fontWeight: "bold",
-    fontSize: 13,
-    lineHeight: 16,
-    color: COLORS.GREEN_PET_ICT,
+    paddingTop: 10,
+    paddingBottom: 10
   },
-  lblRole: {
-    marginTop: 2,
-    marginBottom: 14,
-    paddingHorizontal: 25,
-    fontSize: 10,
-    lineHeight: 13,
-    color: COLORS.GRAYISH_BLUE
+  descriptionText: {
+    fontSize: 14,
+    color: COLORS.GREEN_PET_ICT,
+    textAlign: "justify",
+    overflow: "hidden"
   }
 });
 
-const SpotlightItem = (props) => {
-  const { item, onPressSpotlightItem } = props;
+export const SpotlightItem = (props) => {
+  const { item, onPressSpotlightItem, containerStyle, imageStyle, titleStyle, roleStyle, showDetail } = props;
   return (
     <TouchableOpacity
       elevation={5}
-      style={styles.wrapperItem}
+      style={[styles.wrapperItem, containerStyle]}
       onPress={() => onPressSpotlightItem(item.id)}
     >
       <View style={styles.imageContainer}>
         <Image
-          style={styles.image}
+          style={imageStyle}
           source={{ uri: item.imageUrl }}
           resizeMode="cover"
         />
       </View>
-      <Text style={styles.lblName} numberOfLines={1}>
+      <Text style={titleStyle} numberOfLines={1}>
         {item.spotlightName}
       </Text>
-      <Text style={styles.lblRole} numberOfLines={1}>
+      <Text style={roleStyle} numberOfLines={1}>
         {item.role}
       </Text>
+      {
+        showDetail && (
+          <View style={styles.description}>
+            <Text style={styles.descriptionText}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </Text>
+          </View>
+        )
+      }
     </TouchableOpacity>
   );
 };
@@ -93,10 +99,18 @@ const SpotlightCard = props => {
       <SpotlightItem
         item={spotlight[0]}
         onPressSpotlightItem={spotlightId => onPressSpotlightItem(spotlightId)}
+        containerStyle={{
+          width: WIDTH_SPOTLIGHT_ITEM,
+          margin: 10
+        }}
       />
       <SpotlightItem
         item={spotlight[1]}
-        onPressSpotlightItem={spotlightId => onPressSpotlightItem(spotlightId)}
+        onPressSpotlightItem={spotlightId => onPressSpotlightItem(spotlightId)} 
+        containerStyle={{
+          width: WIDTH_SPOTLIGHT_ITEM,
+          margin: 10
+        }}
       />
     </View>
   );
@@ -109,7 +123,42 @@ SpotlightCard.propTypes = {
 
 SpotlightItem.propTypes = {
   item: PropTypes.object.isRequired,
-  onPressSpotlightItem: PropTypes.func
+  onPressSpotlightItem: PropTypes.func,
+  containerStyle: PropTypes.any,
+  imageStyle: PropTypes.any,
+  titleStyle: PropTypes.any,
+  roleStyle: PropTypes.any,
+  detailMode: PropTypes.bool,
+  showDetail: PropTypes.bool
+};
+
+SpotlightItem.defaultProps = {
+  onPressSpotlightItem: function () {},
+  containerStyle: {
+    width: WIDTH_SPOTLIGHT_ITEM
+  },
+  imageStyle: {
+    width: WIDTH_SPOTLIGHT_ITEM,
+    height: 92
+  },
+  titleStyle: {
+    marginTop: 10,
+    paddingHorizontal: 25,
+    alignSelf: "center",
+    fontWeight: "bold",
+    fontSize: 13,
+    lineHeight: 16,
+    color: COLORS.GREEN_PET_ICT
+  },
+  roleStyle: {
+    marginTop: 2,
+    marginBottom: 14,
+    paddingHorizontal: 25,
+    fontSize: 10,
+    lineHeight: 13,
+    color: COLORS.GRAYISH_BLUE
+  },
+  showDetail: false
 };
 
 export default SpotlightCard;
