12908cf9e5bd783a29cfa4402be311bb97358c4c
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2018-11-21 15:36:43 +0800

    add ParallaxScrollView for profile

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import _ from "lodash";
+import ParallaxScrollView from "react-native-parallax-scroll-view";
 import styles from "./styles";
 import WrapperComponent from "../../components/Wrapper.component";
 import Avatar from "../../components/Avatar";
@@ -13,39 +14,70 @@ class ProfileComponent extends Component {
     this.props.navigation.navigate("EventDetail");
   };
 
-  _renderEventItem = (event, index) => {
+  _renderEventItem = ({ item }) => {
     return (
       <TouchableOpacity
-        key={index}
         style={styles.containerEventItem}
-        onPress={() => this._onPressEventItem(_.get(event, "id"))}
+        onPress={() => this._onPressEventItem(_.get(item, "id"))}
       >
-        <EventCard event={event} withoutBottom />
+        <EventCard event={item} withoutBottom />
       </TouchableOpacity>
     );
   };
 
+  _renderForeground = () => (
+    <View style={styles.foregroundSection}>
+      <View style={styles.containerHeader}>
+        <Avatar user={user} size={84} />
+        <View style={styles.containerInfo}>
+          <Text style={styles.displayName}>{_.get(user, "displayName")}</Text>
+          <Text style={styles.displayPosition}>
+            {_.get(user, "displayPosition")}
+          </Text>
+          <Text style={styles.company}>{_.get(user, "company")}</Text>
+        </View>
+      </View>
+      <Text style={styles.title}>Past Events</Text>
+    </View>
+  );
+
+  _renderStickyHeader = () => (
+    <View style={[styles.containerHeader, styles.stickyHeader]}>
+      <Avatar user={user} size={54} />
+      <View style={styles.containerInfo}>
+        <Text style={styles.displayName}>{_.get(user, "displayName")}</Text>
+        <Text style={styles.displayPosition}>
+          {_.get(user, "displayPosition")}
+        </Text>
+      </View>
+    </View>
+  );
+
   render() {
     return (
       <WrapperComponent>
-        <View style={styles.containerHeader}>
-          <Avatar user={user} size={84} />
-          <View style={styles.containerInfo}>
-            <Text style={styles.displayName}>{_.get(user, "displayName")}</Text>
-            <Text style={styles.displayPosition}>
-              {_.get(user, "displayPosition")}
-            </Text>
-            <Text style={styles.company}>{_.get(user, "company")}</Text>
-          </View>
-        </View>
-        <Text style={styles.title}>Past Events</Text>
-        <FlatList
-          style={styles.containerEvents}
-          data={eventList}
-          keyExtractor={(event, index) => index.toString()}
-          renderItem={this._renderEventItem}
+        <ParallaxScrollView
+          ref={ref => {
+            this.parallaxScrollView = ref;
+          }}
+          backgroundColor={"transparent"}
+          backgroundScrollSpeed={2}
+          fadeOutForeground={true}
+          parallaxHeaderHeight={200}
+          renderForeground={this._renderForeground}
+          stickyHeaderHeight={100}
+          renderStickyHeader={this._renderStickyHeader}
+          contentBackgroundColor={"transparent"}
           showsVerticalScrollIndicator={false}
-        />
+        >
+          <FlatList
+            style={styles.containerEvents}
+            data={eventList}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={this._renderEventItem}
+            showsVerticalScrollIndicator={false}
+          />
+        </ParallaxScrollView>
       </WrapperComponent>
     );
   }
