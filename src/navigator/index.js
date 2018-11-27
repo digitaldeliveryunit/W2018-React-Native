5455cdf5df2d24cd2b46de4c02b97f1b0dc428c4
khoa.trananh <khoa.trananh@petronas.com.my>
2018-11-27 18:04:13 +0800

    UI for onboarding screen

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -3,9 +3,11 @@ import { connect } from "react-redux";
 import { addNavigationHelpers, StackNavigator } from "react-navigation";
 import { createReduxBoundAddListener } from "react-navigation-redux-helpers";
 import MainNavigator from "../navigator/main.navigator";
+import OnBoardingScreen from "../screens/onboarding";
 
 export const AppNavigator = StackNavigator(
   {
+    OnBoarding: { screen: OnBoardingScreen },
     MainScreen: { screen: MainNavigator }
   },
   {
---
+++ src/screens/onboarding/styles.js
@@ -0,0 +1,65 @@
+import { StyleSheet } from "react-native";
+import { CommonStyles, COLORS } from "../../helpers/common-styles";
+import { sizeWidth, sizeHeight, sizeFont } from "../../helpers/size.helper";
+
+export default StyleSheet.create({
+  swiper: {
+  },
+  dotNormal: {
+    backgroundColor: "#fff",
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginLeft: 4,
+    marginRight: 4,
+    opacity: 0.3
+  },
+  dotActive: {
+    backgroundColor: "#fff",
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginLeft: 4,
+    marginRight: 4
+  },
+  onboarding: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: sizeHeight(15)
+  },
+  image: {
+    width: sizeWidth(90),
+    height: sizeWidth(90),
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 40
+  },
+  title: {
+    color: "#FFF",
+    fontSize: sizeFont(6),
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  content: {
+    color: "#FFF",
+    fontSize: sizeFont(4),
+    textAlign: "center",
+    paddingLeft: sizeWidth(8),
+    paddingRight: sizeWidth(8)
+  },
+  bottomRow: {
+    width: sizeWidth(100),
+    paddingLeft: sizeWidth(5),
+    paddingRight: sizeWidth(5),
+    position: "absolute",
+    bottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }, 
+  bottomButton: {
+  },
+  bottomText: {
+    color: "#FFF",
+    fontSize: 16
+  }
+});
