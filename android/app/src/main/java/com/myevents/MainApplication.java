09dbd8f94c4bf6a6a73a989c569c7864981476e6
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2018-11-29 01:09:31 +0800

    add event calendar for event details

--- android/app/src/main/java/com/myevents/MainApplication.java
+++ android/app/src/main/java/com/myevents/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.remobile.toast.RCTToastPackage;
+import com.calendarevents.CalendarEventsPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CalendarEventsPackage(),
             new VectorIconsPackage(),
             new FastImageViewPackage(),
             new RCTToastPackage()
